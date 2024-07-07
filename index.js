09a744a2bcb8ba1152dfb32f688732f03f63a684
Kevo tushap <59371076+kevotushap@users.noreply.github.com>
2024-07-07 13:51:09 +0300

    port 3000 index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/api/hello", async (req, res) => {
 })
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening at PORT ${PORT}`)
 })
